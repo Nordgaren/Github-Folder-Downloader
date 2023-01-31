fb3f82711f18ae4124086fadc12223286431b4d2
Nordgaren <nordgarentv@gmail.com>
2023-01-31 00:12:45 -0700

    Added support for downloading a single file

--- README.md
+++ README.md
@@ -12,10 +12,28 @@ There are two required args. The repo and the folder.
 The repo will be `username/reponame` ex: `nordgaren/Github-Folder-Downloader` and the folder will be the path from the root directory of that repo, 
 ex: `.idea/inspectionProfiles` with no slash on the end. The folder structure of the repo will be recreated, but only the files in the folder specify will be downloaded.  
 
-Example command: 
+## Usage: 
+### gitdl.py [-h] [-o] [-r] [-f] repo path
+```
+positional arguments:
+  repo               The repo where the file or folder is stored
+  path               The folder or file you want to download
+
+options:
+  -h, --help         show this help message and exit
+  -o OUT, --out OUT  path to folder you want to downoad to.
+  -r, --recursive    Recursively download directories. Folder downloads, only!
+  -f, --file         set flag to download a single file, instead of a folder.
+```
+
+Folder - path must not end with a slash.
 ```sh
 python gitdl.py nordgaren/Github-Folder-Downloader .idea/inspectionProfiles
 ```  
+Single File
+```sh
+python gitdl.py nordgaren/Github-Folder-Downloader -f .idea/.gitignore
+```
 
 The other two optional arguments are:  
 `-o` or `--output` which lets you specify an output path (the defualt is `./downloads/`)  
--- gitdl.py
+++ gitdl.py
@@ -1,7 +1,16 @@
 import os
-from github import Github, Repository
+from github import Github, Repository, ContentFile
 import requests
 from argparse import ArgumentParser, Namespace
+
+
+def download(c: ContentFile, out: str):
+    r = requests.get(c.download_url)
+    output_path = f'{out}/{c.path}'
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    with open(output_path, 'wb') as f:
+        print(f'downloading {output_path}')
+        f.write(r.content)
 
 
 def download_folder(repo: Repository, folder: str, out: str, recursive: bool):
@@ -11,20 +20,24 @@ def download_folder(repo: Repository, folder: str, out: str, recursive: bool):
             if recursive:
                 download_folder(repo, c.path, out, recursive)
             continue
-        r = requests.get(c.download_url)
-        output_path = f'{out}/{c.path}'
-        os.makedirs(os.path.dirname(output_path), exist_ok=True)
-        with open(output_path, 'wb') as f:
-            print(f'downloading {output_path}')
-            f.write(r.content)
+        download(c, out)
+
+
+def download_file(repo: Repository, folder: str, out: str):
+    c = repo.get_contents(folder)
+    download(c, out)
 
 
 def get_args() -> Namespace:
     parser = ArgumentParser()
-    parser.add_argument('repo')
-    parser.add_argument('folder')
-    parser.add_argument('-o', '--out', default='downloads', required=False)
-    parser.add_argument('-r', '--recursive', action='store_true')
+    parser.add_argument('repo', help='The repo where the file or folder is stored')
+    parser.add_argument('path', help='The folder or file you want to download')
+    parser.add_argument('-o', '--out', default='downloads', required=False, help='Path to folder you want to downoad '
+                                                                                 'to.')
+    parser.add_argument('-r', '--recursive', action='store_true', help='Recursively download directories. Folder '
+                                                                       'downloads, only!')
+    parser.add_argument('-f', '--file', action='store_true', help='Set flag to download a single file, instead of a '
+                                                                  'folder.')
     return parser.parse_args()
 
 
@@ -32,4 +45,7 @@ if __name__ == '__main__':
     args = get_args()
     g = Github()
     repo = g.get_repo(args.repo)
-    download_folder(repo, args.folder, args.out, args.recursive)
+    if args.file:
+        download_file(repo, args.path, args.out)
+    else:
+        download_folder(repo, args.path, args.out, args.recursive)
